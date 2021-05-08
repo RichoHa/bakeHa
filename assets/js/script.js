ef8409e619daad3b1348e6dfc90962751fcd5483
Richo Ha <richard.dqh@gmail.com>
2021-05-08 15:57:58 +0800

    Completed first card

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// const clientID = `521322355544283`;
+// const redirectURI = `https://richoha.github.io/bakeHa/`;
+
+// const token = 'IGQVJYS0hCaHZABU0U5MzB4RF9sdkhRTlp2Wl84SlNmMWZAnN2tNZADBfelpZARTNhTkluTWZA2c0lqSGxRQ3BYZAThzVWdtaFJoVGFZAMjdUNG5xU0pZAYTh5TlQzUUoza0YzaDVBckxwWG1OZAEZA1ejdoT0hybgZDZD'
+// getPics()
+
+
+// function getPics() {
+//     var requestUrl = `https://graph.instagram.com/me/media?fields=caption,id,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=${token};`
+//     fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//         var img = document.getElementById("img");
+//         img.src = data.data[0].media_url;
+//     })
+// }
